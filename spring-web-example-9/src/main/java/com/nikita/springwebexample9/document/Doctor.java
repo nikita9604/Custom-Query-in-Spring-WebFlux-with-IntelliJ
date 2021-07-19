797bbcd671103ce50c47e701a7a334f5bc41f1fd
Nikita Rath <54584131+nikita9604@users.noreply.github.com>
2021-07-19 17:24:43 +0530

    Add files via upload

---
+++ spring-web-example-9/src/main/java/com/nikita/springwebexample9/document/Doctor.java
@@ -0,0 +1,18 @@
+package com.nikita.springwebexample9.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Doctor {
+    @Id
+    private int did;
+    private String dname;
+    private String specs;
+    private int pid;
+}
